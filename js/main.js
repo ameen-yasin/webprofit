7ab56a6dc9ca1d9c7aab3c015f84356edfd63cfa
ameen-yasin <ameeen_1998@hotmail.com>
2021-03-21 15:47:25 +0200

    last

--- js/main.js
+++ js/main.js
@@ -10,14 +10,12 @@ function closeNav() {
 
 
 // Form Validation
+
 // This fuction will retrn refrance of the html element
 function refranceElement(elemnetId) {
     return document.getElementById(elemnetId);
 }
 
-function inputRefracne(inputName) {
-    return document.forms["contact-form"][inputName];
-}
 
 // Retrieving the values of form elements
 const firstName = inputRefracne("first-name");
@@ -29,22 +27,18 @@ const companyWebsite = inputRefracne("company-website");
 const jobTitle = inputRefracne("job-title");
 const companySize = inputRefracne("company-size");
 
-const firstNameValue = firstName.value;
-const lastNameValue = lastName.value;
-const emailValue = email.value;
-const phoneNumberValue = phoneNumber.value;
-const companyNameValue = companyName.value;
-const companyWebsiteValue = companyWebsite.value;
-const jobTitleValue = jobTitle.value;
-const companySizeValue = companySize.value;
-
-
 function validation(refranceElement, eventListener, className) {
     refranceElement.addEventListener(eventListener, () => {
-        // if the value of the element is empty
+        // if the value of the element is empty\
+        alert("test");
+
         if (refranceElement.value === "") {
-            // we add error class to the element
+            alert("entered");
+            // we add error class to the element if the user didnt enter anything
             refranceElement.classList.add(className);
+            // } else if (refranceElement.validity.patternMismatch) {
+            //     // if the user enter invalid pattern 
+            //     refranceElement.classList.add(className);
         } else {
             // if the field is valid, we remove the error message.
             refranceElement.classList.remove(className);
@@ -52,33 +46,20 @@ function validation(refranceElement, eventListener, className) {
     });
 }
 
-// Validate telephone numbers
-function validateNumber(eventListener) {
-    phoneNumber.addEventListener(eventListener, () => {
-
-        // var regex = /^[0-9]\d{6}$/;
-        // if (regex.test(phoneNumberValue) == false) {
-        //     alert("enterd");
-        //     phoneNumber.classList.add('error');
-        // }
-        // // if the value of the element is empty
-
-        if (phoneNumberValue === "") {
-            // we add error class to the element
-            phoneNumber.classList.add('error');
-        } else {
-            // in case the phone number less than 6 digit we add
-            // error clas to phone element
-            var regex = /[0-9]{6}/;
-            if (regex.test(phoneNumberValue) == false) {
-                phoneNumber.classList.add('error');
-            } else {
-                // if the field is valid, we remove the error message.
-                refranceElement.classList.remove(className);
-            }
-        }
-    });
+function validationOnSubmit(refranceElement, className) {
+    // if the value of the element is empty
+    if (refranceElement.value === "") {
+        // we add error class to the element
+        refranceElement.classList.add(className);
+    } else if (refranceElement.validity.patternMismatch) {
+        // if the user enter invalid pattern 
+        refranceElement.classList.add(className);
+    } else {
+        // if the field is valid, we remove the error message.
+        refranceElement.classList.remove(className);
+    }
 }
+
 
 // Each time the user types something, we check if the
 // form fields are valid.
@@ -91,8 +72,8 @@ validation(lastName, 'focusout', 'error');
 validation(email, 'input', 'error');
 validation(email, 'focusout', 'error');
 
-validateNumber('input');
-validateNumber('focusout');
+validation(phoneNumber, 'input', 'error');
+validation(phoneNumber, 'focusout', 'error');
 
 validation(companyName, 'input', 'error');
 validation(companyName, 'focusout', 'error');
@@ -110,58 +91,17 @@ validation(companySize, 'focusout', 'company-size-error');
 
 
 
-function validateForm() {
-    let boolean = true;
-    if (firstNameValue == "") {
-        firstName.classList.add('error');
-        boolean = false;
-    }
-    if (lastNameValue === "") {
-        lastName.classList.add('error');
-        boolean = false;
-    }
-    // if (emailValue == "") {
-    //     email.classList.add('error');
-    //     boolean = false;
-    // } else {
-    //     // Regular expression for basic email validation
-    //     var regex = /^\S+@\S+\.\S+$/;
-    //     if (regex.test(emailValue) == false) {
-    //         email.classList.add('error');
-    //         boolean = false;
-    //     }
-    // }
-
-    // Validate telephone numbers
-    if (phoneNumberValue == "") {
-        phoneNumber.classList.add('error');
-        boolean = false;
-    } else {
-        var regex = /^[0-9]\d{6}$/;
-        if (regex.test(phoneNumberValue) == false) {
-            phoneNumber.classList.add('error');
-            boolean = false;
-        }
-    }
-    if (companyNameValue == "") {
-        companyName.classList.add('error');
-        boolean = false;
-    }
-    if (companyWebsiteValue == "") {
-        companyWebsite.classList.add('error');
-        boolean = false;
-    }
-    if (jobTitleValue == "") {
-        jobTitle.classList.add('error');
-        boolean = false;
-    }
-    if (companySizeValue == "") {
-        companySize.classList.add('company-size');
-        boolean = false;
-    }
-
-    return boolean;
-}
+// function validateForm(e) {
+//     e.preventDefault()
+//     validationOnSubmit(firstName, 'error');
+//     validationOnSubmit(lastName, 'error');
+//     validationOnSubmit(email, 'error');
+//     validationOnSubmit(phoneNumber, 'error');
+//     validationOnSubmit(companyName, 'error');
+//     validationOnSubmit(companyWebsite, 'error');
+//     validationOnSubmit(jobTitle, 'error');
+//     validationOnSubmit(companySize, 'company-size-error');
+// }
 
 
 // email.addEventListener('blur', () => {
